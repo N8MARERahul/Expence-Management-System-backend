c681829bbd48760ed6779674156dcc14d008a5e1
N8MARERahul <nightmarecoding0001@gmail.com>
2024-10-14 16:52:45 +0530

    user router added

--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -1,13 +1,12 @@
 import { Router } from "express"
 import { addIncome, getIncomes, deleteIncome } from "../controllers/income.controller.js"
 import { addExpense, getExpenses, deleteExpense } from "../controllers/expense.controller.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send("Hello World")
-})
+router.use(verifyJWT)
 
 router.post('/add-income', addIncome)
     .get('/get-incomes', getIncomes)
---
+++ src/routes/users.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { changeCurrentPassword, getCurrentUser, loginUser, refreshAccessToken, registerUser, updateAccountDetails } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router()
+
+router.route("/register").post(registerUser)
+router.route("/login").post(loginUser)
+router.route("/logout").post(verifyJWT, loginUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+export default router;
